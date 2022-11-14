a4748a3fcb23095b198901431c8a19698c817ac2
Dmitry Vystropov <beepper210980@gmail.com>
2022-11-14 22:02:36 +0300

    Динамический вывод графиков из БД

--- resources/js/components/admin/home/index.vue
+++ resources/js/components/admin/home/index.vue
@@ -7,7 +7,10 @@
 
 
     <div class="container">
-        <Line :chart-data="chartData" :height="100" :options="{responsive: true, maintainAspectRatio: true}" />
+        <div :class="{'d-flex justify-content-center mt-5': !loaded}">
+            <div :class="{'spinner-border text-primary': !loaded} " role="status"> </div> <!-- ! Значек процесса загрузки -->
+        </div>
+        <Line v-if="loaded"  :chart-data="chartData" :height="100" :options="{responsive: true, maintainAspectRatio: true}"/>
 
     </div>
 
@@ -21,38 +24,106 @@
 // ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 import 'chart.js/auto';
-import { Line } from 'vue-chartjs'
+import {Line} from 'vue-chartjs'
 
 export default {
     name: 'BarChart',
-    components: { Line },
+    components: {Line},
+
     data() {
         return {
-            chartData: {
-                labels: [
-                    'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                ],
-                datasets: [
-                    {
-                        label: 'Data 1',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: [0, 10, 5, 2, 20, 30, 45],
-                    },
-                    {
-                        label: 'Data 0',
-                        backgroundColor: 'rgb(25, 99, 132)',
-                        borderColor: 'rgb(25, 99, 132)',
-                        data: [10, 15, 15, 20, 2, 5, 4],
-                    }
-                ]
-            }
+            loaded: false,   // ! Признак загрузки данных //
+
+            pointData: [],
+            // chartData: {
+                // labels: ['January', 'February', 'March', 'April', 'May', 'June',  ],
+                // labels: ['2022-09-01 00:00:00.000', '2022-09-01 02:00:00.000', '2022-09-01 04:00:00.000', '2022-09-01 06:00:00.000', '2022-09-01 08:00:00.000', '2022-09-01 10:00:00.000',],
+                // datasets: [
+                //     {
+                //         label: 'Data 1',
+                //         backgroundColor: 'rgb(255, 99, 132)',
+                //         borderColor: 'rgb(255, 99, 132)',
+                        // data: [0, 10, 5, 2, 20, 30, 45],
+                        // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,]
+                        // data: this.pointData.map(n => n.PI7026A),
+            //         },
+            //     ]
+            // }
         }
+    },
+
+    async mounted() {
+        this.loadPointData();
+
+    },
+
+    methods: {
+        loadPointData: function () {
+            axios.get('/api/dk_a_data')
+                .then((response) => {
+                    this.pointData = response.data.data;
+                    // this.loading = false;
+                    this.loaded = true
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+    computed: {
+        chartData() {
+            return {
+                    // labels: ['2022-09-01 00:00:00.000', '2022-09-01 02:00:00.000', '2022-09-01 04:00:00.000', '2022-09-01 06:00:00.000', '2022-09-01 08:00:00.000', '2022-09-01 10:00:00.000',],
+                    labels: this.pointData.map(n => n.LocalTime),
+                    datasets: [
+                        {
+                            label: 'PI7026A',
+                            backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,],
+                            data: this.pointData.map(n => +n.PI7026A),
+                            borderWidth: 1,
+                            pointRadius: 0,
+                            yAxisID: 'left-y-axis'
+                        },
+                        {
+                            label: 'PI7028A',
+                            backgroundColor: 'rgb(79,156,243)',
+                            borderColor: 'rgb(79,156,243)',
+                            // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,],
+                            data: this.pointData.map(n => +n.PI7028A),
+                            borderWidth: 1,
+                            pointRadius: 0,
+                            yAxisID: 'left-y-axis'
+                        },
+                        {
+                            label: 'PDI7013A',
+                            backgroundColor: 'rgb(52,208,42)',
+                            borderColor: 'rgb(52,208,42)',
+                            // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,],
+                            data: this.pointData.map(n => +n.PDI7013A),
+                            borderWidth: 1,
+                            pointRadius: 0,
+                            yAxisID: 'right-y-axis'
+                        },
+                    ],
+
+                options: {
+                    scales: {
+                        'left-y-axis': {
+                            type: 'linear',
+                            position: 'left'
+                        },
+                        'right-y-axis': {
+                            type: 'linear',
+                            position: 'right'
+                        }
+                    }
+                }
+            }
+            /* mutable chart data */
+        },
     }
 }
 </script>
--- resources/js/components/pages/home/DK_A.vue
+++ resources/js/components/pages/home/DK_A.vue
@@ -1,12 +1,36 @@
 <template>
     <h2> Data 111 DK A</h2>
-    <div class="spinner-border" role="status">
-        <span class="sr-only"></span>
+    <div :class="{'spinner-border': loading}" role="status">
+<!--        <span class="sr-only">Load ...</span>-->
     </div>
-    <div class="list-group">
-        <a class="list-group-item" v-for="point in pointData">
-            {{point.PI7026A}}
-        </a>
+    <div class="list-group col-1">
+<!--        <a class="list-group-item" v-for="point in pointData">-->
+<!--            {{Math.floor(point.PI7026A*100)/100}}-->
+<!--        </a>-->
+    </div>
+
+    <div >
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">Data</th>
+                <th scope="col">PI7026A</th>
+                <th scope="col">PI7028A</th>
+                <th scope="col">PDI7013A</th>
+                <th scope="col">ZVI7000A</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="point in pointData">
+                <td>{{point.LocalTime}}</td>
+                <td>{{Math.floor(point.PI7026A*100)/100}}</td>
+                <td>{{Math.floor(point.PI7028A*100)/100}}</td>
+                <td>{{Math.floor(point.PDI7013A*100)/100}}</td>
+                <td>{{Math.floor(point.ZVI7000A*100)/100}}</td>
+            </tr>
+
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -16,6 +40,7 @@ export default {
 
     data: function () {
         return {
+            loading: true,
             pointData: [],
         }
     },
@@ -29,8 +54,9 @@ export default {
             axios.get('/api/dk_a_data')
                 .then((response) => {
                     this.pointData = response.data.data;
+                    this.loading = false;
 
-                    console.log(response.data.data)
+                    // console.log(response.data.data)
                 })
                 .catch(function (error){
                     console.log(error);
