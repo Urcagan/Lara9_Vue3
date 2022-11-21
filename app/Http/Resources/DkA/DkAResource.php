575ffd28df3c42c0acde59aee6d1f2741fb22de6
Vystropov <beepper210980@gmail.com>
2022-11-21 16:42:25 +0300

    Добавлена обработка данных после api запроса

--- app/Http/Resources/DkA/DkAResource.php
+++ app/Http/Resources/DkA/DkAResource.php
@@ -19,20 +19,27 @@ class DkAResource extends JsonResource
             'LocalTime' => $this->LocalTime,
             'PI7026A' => $this->PI7026A,
             'PI7028A' => $this->PI7028A,
+//            'PI7024A' => $this->PI7024A,
+
             'PDI7013A' => $this->PDI7013A,
+
             'ZVI7000A' => $this->ZVI7000A,
             'ZVI7001A' => $this->ZVI7001A,
             'ZVI7002A' => $this->ZVI7002A,
             'ZVI7003A' => $this->ZVI7003A,
+
             'KVI7000A' => $this->KVI7000A,
+
             'XVI7000A' => $this->XVI7000A,
             'XVI7001A' => $this->XVI7001A,
             'XVI7002A' => $this->XVI7002A,
             'XVI7003A' => $this->XVI7003A,
+
             'XVI7004A' => $this->XVI7004A,
             'XVI7005A' => $this->XVI7005A,
             'XVI7006A' => $this->XVI7006A,
             'XVI7007A' => $this->XVI7007A,
+
             'TI7006A' => $this->TI7006A,
             'TI7007A' => $this->TI7007A,
             'TI7008A' => $this->TI7008A,
--- resources/js/components/DK_A/home/index.vue
+++ resources/js/components/DK_A/home/index.vue
@@ -5,13 +5,12 @@
         <router-link to="/">Home page</router-link>
     </div>
 
-
     <div class="container">
         <div :class="{'d-flex justify-content-center mt-5': !loaded}">
             <div :class="{'spinner-border text-primary': !loaded} " role="status"></div>
             <!-- ! Значек процесса загрузки -->
         </div>
-        <Line v-if="loaded" :chart-data="chartData" :height="100" :options="chartOptions" />
+        <Line v-if="loaded" :chart-data="chartData" :height="100" :options="chartOptions"/>
 
     </div>
 
@@ -26,57 +25,113 @@
 
 import 'chart.js/auto';
 import {Line} from 'vue-chartjs'
-import {Chart , Title} from "chart.js";
+import {Chart, Title} from "chart.js";
 
+Chart.register(Title)
 Chart.defaults.elements.line.borderWidth = 1 // Задает глобальное значение толщины всех линий
 Chart.defaults.elements.point.radius = 1  // Задает глобальное значение радиуса точки
+// Chart.defaults.backgroundColor = 'rgb(0,0,0)'
 
 export default {
-    name: 'BarChart',
+    name: 'LineChart',
     components: {Line},
 
     data() {
         return {
             loaded: false,   // ! Признак загрузки данных //
+            // chartData: null,
 
-            pointData: [],
-            // chartData: {
-            // labels: ['January', 'February', 'March', 'April', 'May', 'June',  ],
-            // labels: ['2022-09-01 00:00:00.000', '2022-09-01 02:00:00.000', '2022-09-01 04:00:00.000', '2022-09-01 06:00:00.000', '2022-09-01 08:00:00.000', '2022-09-01 10:00:00.000',],
-            // datasets: [
-            //     {
-            //         label: 'Data 1',
-            //         backgroundColor: 'rgb(255, 99, 132)',
-            //         borderColor: 'rgb(255, 99, 132)',
-            // data: [0, 10, 5, 2, 20, 30, 45],
-            // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,]
-            // data: this.pointData.map(n => n.PI7026A),
-            //         },
-            //     ]
-            // }
-            labels: [],
+            labelsData: [],
+            dataPiont: [],
+
+            localTime: [],
+            PI7026A: [],
+            PI7028A: [],
+            PDI7013A: [],
+            ZVI7000A: [],
+            ZVI7001A: [],
+            ZVI7002A: [],
+            ZVI7003A: [],
+            KVI7000A: [],
+            XVI7000A: [],
+            XVI7001A: [],
+            XVI7002A: [],
+            XVI7003A: [],
+            XVI7004A: [],
+            XVI7005A: [],
+            XVI7006A: [],
+            XVI7007A: [],
+            TI7006A: [],
+            TI7007A: [],
+            TI7008A: [],
+            TI7009A: [],
+            TI7010A: [],
+            TI7011A: [],
+            TI7012A: [],
+            TI7013A: [],
+            TI7014A: [],
+            TI7015A: [],
+            TI7043A: [],
+            LI7008A: [],
 
         }
     },
 
     async mounted() {
-        this.loadPointData();
+        this.loaded = false
 
+        this.loadPointData();
     },
 
     methods: {
         loadPointData: function () {
+
+            if(this.chartData != null) {
+                this.chartData.des
+            }
+
             axios.get('/api/dk_a_data')
                 .then((response) => {
                     this.pointData = response.data.data;
-                    // this.loading = false;
-                    this.loaded = true
-                    this.labels = this.pointData.map(n => n.LocalTime);
-                    console.log(this.labels.length)
+
+                    this.localTime = response.data.data.map(item => { return item.LocalTime });
+                    this.PI7026A = response.data.data.map(item => { return item.PI7026A });
+                    this.PI7028A = response.data.data.map(item => { return item.PI7028A });
+                    this.PDI7013A = response.data.data.map(item => { return item.PDI7013A });
+                    this.ZVI7000A= response.data.data.map(item => { return item.ZVI7000A });
+                    this.ZVI7001A= response.data.data.map(item => { return item.ZVI7001A });
+                    this.ZVI7002A= response.data.data.map(item => { return item.ZVI7002A });
+                    this.ZVI7003A= response.data.data.map(item => { return item.ZVI7003A });
+                    this.KVI7000A= response.data.data.map(item => { return item.KVI7000A });
+                    this.XVI7000A= response.data.data.map(item => { return item.XVI7000A });
+                    this.XVI7001A= response.data.data.map(item => { return item.XVI7001A });
+                    this.XVI7002A= response.data.data.map(item => { return item.XVI7002A });
+                    this.XVI7003A= response.data.data.map(item => { return item.XVI7003A });
+                    this.XVI7004A= response.data.data.map(item => { return item.XVI7004A });
+                    this.XVI7005A= response.data.data.map(item => { return item.XVI7005A });
+                    this.XVI7006A= response.data.data.map(item => { return item.XVI7006A });
+                    this.XVI7007A= response.data.data.map(item => { return item.XVI7007A });
+                    this.TI7006A= response.data.data.map(item => { return item.TI7006A });
+                    this.TI7007A= response.data.data.map(item => { return item.TI7007A });
+                    this.TI7008A= response.data.data.map(item => { return item.TI7008A });
+                    this.TI7009A= response.data.data.map(item => { return item.TI7009A });
+                    this.TI7010A= response.data.data.map(item => { return item.TI7010A });
+                    this.TI7011A= response.data.data.map(item => { return item.TI7011A });
+                    this.TI7012A= response.data.data.map(item => { return item.TI7012A });
+                    this.TI7013A= response.data.data.map(item => { return item.TI7013A });
+                    this.TI7014A= response.data.data.map(item => { return item.TI7014A });
+                    this.TI7015A= response.data.data.map(item => { return item.TI7015A });
+                    this.TI7043A= response.data.data.map(item => { return item.TI7043A });
+                    this.LI7008A= response.data.data.map(item => { return item.LI7008A });
+
+                    // console.log(this.PI7026A)
+
+                    // this.labelsData = this.pointData.map(n => n.LocalTime);
                 })
                 .catch(function (error) {
                     console.log(error);
                 })
+                .finally(() => (this.loaded = true))
         }
     },
 
@@ -84,42 +139,107 @@ export default {
 
         chartData() {
             return {
-                // labels: ['2022-09-01 00:00:00.000', '2022-09-01 02:00:00.000', '2022-09-01 04:00:00.000', '2022-09-01 06:00:00.000', '2022-09-01 08:00:00.000', '2022-09-01 10:00:00.000',],
-                labels: this.pointData.map(n => n.LocalTime),
+                labels: this.localTime,
+                // labels: this.pointData.map(n => n.LocalTime),
 
                 datasets: [
-
-
                     {
                         label: 'PI7026A',
-                        backgroundColor: '#FF6384FF',
-                        borderColor: '#FF6384FF',
-                        // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,],
-                        data: this.pointData.map(n => +n.PI7026A),
+                        backgroundColor: '#63faff',
+                        borderColor: '#63FAFFFF',
+                        data: this.PI7026A,
+                        // data: this.pointData.map(n => +n.PI7026A),
                         // borderWidth: 1,
                         // radius: 0, // Задает радиус точки
                         yAxisID: 'left-y-axis'
                     },
                     {
                         label: 'PI7028A',
-                        backgroundColor: 'rgb(79,156,243)',
-                        borderColor: 'rgb(79,156,243)',
-                        // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,],
-                        data: this.pointData.map(n => +n.PI7028A),
-                        // borderWidth: 1,
-                        // radius: 0,
+                        backgroundColor: 'rgb(245,235,89)',
+                        borderColor: 'rgb(245,235,89)',
+                        data: this.PI7028A ,
                         yAxisID: 'left-y-axis'
                     },
                     {
                         label: 'PDI7013A',
                         backgroundColor: 'rgb(52,208,42)',
                         borderColor: 'rgb(52,208,42)',
-                        // data: [1.6919258832931519, 1.6673653125762939, 1.6467424631118774, 1.6274768114089966, 1.6357301473617554, 1.6665645837783813,],
-                        data: this.pointData.map(n => +n.PDI7013A),
-                        // borderWidth: 1,
-                        // radius: 0,
+                        data: this.PDI7013A,
                         yAxisID: 'right-y-axis'
                     },
+                    {
+                        label: 'ZVI7000A',
+                        backgroundColor: '#2ad0b2',
+                        borderColor: '#D0B42AFF',
+                        data: this.ZVI7000A,
+                        yAxisID: 'right-y-axis'
+                    },
+                    {
+                        label: 'ZVI7001A',
+                        backgroundColor: '#D0B42AFF',
+                        borderColor: '#D0B42AFF',
+                        data: this.ZVI7001A,
+                        yAxisID: 'right-y-axis'
+                    },
+                    {
+                        label: 'ZVI7002A',
+                        backgroundColor: '#D0B42AFF',
+                        borderColor: '#D0B42AFF',
+                        data: this.ZVI7002A,
+                        yAxisID: 'right-y-axis'
+                    },
+                    {
+                        label: 'ZVI7003A',
+                        backgroundColor: '#D0B42AFF',
+                        borderColor: '#D0B42AFF',
+                        data: this.ZVI7003A,
+                        yAxisID: 'right-y-axis'
+                    },
+                    {
+                        label: 'KVI7000A',
+                        backgroundColor: '#D0B42AFF',
+                        borderColor: '#D0B42AFF',
+                        data: this.KVI7000A,
+                        yAxisID: 'right-y-axis'
+                    },
+                    {
+                        label: 'XVI7000A',
+                        backgroundColor: '#D0B42AFF',
+                        borderColor: '#D0B42AFF',
+                        data: this.XVI7000A,
+                        yAxisID: 'right-y-axis'
+                    },
+                    {
+                        label: 'XVI7001A',
+                        backgroundColor: '#D0B42AFF',
+                        borderColor: '#D0B42AFF',
+                        data: this.XVI7001A,
+                        yAxisID: 'right-y-axis'
+                    },
+
+
+
+
+
+                    // XVI7002A
+                    // XVI7003A
+                    // XVI7004A
+                    // XVI7005A
+                    // XVI7006A
+                    // XVI7007A
+                    // TI7006A
+                    // TI7007A
+                    // TI7008A
+                    // TI7009A
+                    // TI7010A
+                    // TI7011A
+                    // TI7012A
+                    // TI7013A
+                    // TI7014A
+                    // TI7015A
+                    // TI7043A
+                    // LI7008A
+
                 ],
             }
             /* mutable chart data */
@@ -127,6 +247,10 @@ export default {
 
         chartOptions() {
             return {
+                title: {
+                    display: true,
+                    text: 'МГК 111-Н1А'
+                },
                 responsive: true,
                 maintainAspectRatio: true,
                 scales: {
@@ -138,10 +262,7 @@ export default {
                         type: 'linear',
                         position: 'right'
                     },
-
                 },
-
-
             }/* mutable chart options */
         },
 
