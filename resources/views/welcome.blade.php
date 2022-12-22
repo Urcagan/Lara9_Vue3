4c606cfe8fde1de042d252b1e361aa5f44e803d6
Dmitry Vystropov <beepper210980@gmail.com>
2022-12-22 21:48:35 +0300

    Добавлен Display Flex

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 - Vue 3</title>
+        <title>Reg Mex</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
