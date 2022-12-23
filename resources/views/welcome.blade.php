8994bdec66a3c1441babe1470ac491e166b7c229
Vystropov <beepper210980@gmail.com>
2022-12-23 08:06:33 +0300

    Слияние

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 9 - Vue 3</title>
+        <title>RegMex</title>
 
         <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> -->
         <!-- Scripts -->
         @vite('resources/js/app.js')
-{{--        @vite(['resources/css/app.css','resources/js/app.js'])--}}
-
-
 
     </head>
     <body style="background-color: #bebed5">
